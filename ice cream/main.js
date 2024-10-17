f3d000ac4a3a387164810ed3c18d78d280832f0d
Kay <mccray382@gmail.com>
2024-10-17 15:12:24 -0400

    wip

---
+++ ice cream/main.js
@@ -0,0 +1,63 @@
+function getReceipt(
+    numberOfScoops,
+    isCup,
+    wantsSprinkles,
+    wantsWhipped,
+    wantsFudge,
+    wantsCherry
+){
+    const taxRate = 0.08;
+    const basePrice = 1.00;
+    const costPerScoop = 1.25;
+    let total = basePrice + (costPerScoop * numberOfScoops);
+    if(isCup) {
+        if(wantsSprinkles){
+            total += 0.5
+        }
+        if(wantsWhipped){
+            total += 0.25
+        }
+        if(wantsFudge){
+            total += 1.25
+        }
+        if(wantsCherry){
+            total += 0.25
+        }
+    }
+    const taxAmount = total * taxRate;
+    const totalDue = total + taxAmount;
+    return `
+        Base Price: $${total.toFixed(2)}
+        Tax: $${taxAmount.toFixed(2)}
+        Total Due: $${totalDue.toFixed(2)}
+    `;
+}
+
+function onContainerClicked(e) {
+    toppings.style.display = cup.checked ? "block" : "none";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scoops = document.getElementById("scoops");
+    const cup = document.getElementById("cup");
+    const cone = document.getElementById("cone");
+    const sprinkles = document.getElementById("sprinkles");
+    const whipped = document.getElementById("whipped");
+    const fudge = document.getElementById("fudge");
+    const cherry = document.getElementById("cherry");
+
+    cup.addEventListener("click", onContainerClicked);
+    cone.addEventListener("click", onContainerClicked);
+    
+    submitOrder.addEventListener("click", () => {
+        outputReceipt.innerText = getReceipt(
+            Number(scoops.value),
+            cup.checked,
+            sprinkles.checked,
+            whipped.checked,
+            fudge.checked,
+            cherry.checked
+        
+        );
+    })
+});
